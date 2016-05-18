0478ace41e8cb8cc2345541bf25a6a83000cb4ca
Rioscorona <brenda.dsaupoo@gmail.com>
2016-05-17 23:17:10 -0500

    3subida

--- js/acciones/acciones4.js
+++ js/acciones/acciones4.js
@@ -1,30 +1,29 @@
 // JavaScript Document
 
- $(document).ready(function (e){
-	document .addEventListener ("deviceready",function(){
-		$('#disp table td').eq(3).text(devide.model);
-		$('#disp table td').eq(5).text(devide.cordova);
-		$('#disp table td').eq(7).text(devide.platform);
-		$('#disp table td').eq(9).text(devide.version);
-		$('#disp table td').eq(11).text(devide.uuid);
-	document.addEvenlistener("pause",function(){
-		escribehistoria ('la app se pausó');
-	},false);
-	document.addEvenlistener("resume",function(){
-		escribehistoria ('la app se reinició');
-	},false);
-	document.addEvenlistener("online",function(){
-		escribehistoria ('la app se conecto a la red');
-	},false);
-	document.addEvenlistener("offline",function(){
-		escribehistoria ('la app se desconecto a la red');
-	},false);
+$(document).ready(function (e){
+    document .addEventListener("deviceready",function(){
+		$('#disp table td').eq(3).text(device.model);
+		$('#disp table td').eq(5).text(device.cordova);
+		$('#disp table td').eq(7).text(device.platform);
+		$('#disp table td').eq(9).text(device.version);
+		$('#disp table td').eq(11).text(device.uuid);
+		document.addEventListener("pause",function(){
+			escribehistoria ('la app se pauso');
+		},false);//pause
+		document.addEventListener("resume",function(){
+			escribehistoria ('la app se reinicio');
+		},false);//resume
+		document.addEventListener("online",function(){
+			escribehistoria ('la app se conecto a la red');
+		},false);//se conecto
+		document.addEventListener("offline",function(){
+			escribehistoria ('la app se desconecto de la red');
+		},false);//se desconecto
+		
+	},false);//ready device
 	
-	},false);
-	
- });
- 
- function escribehistoria(accion){
-	 $('#eHistoria').append('<li>'+accion+'</li>');
- }
-	
+});//document
+
+function escribehistoria(accion){
+	$('#eHistoria').append('<li>'+accion+'</li>');
+}
